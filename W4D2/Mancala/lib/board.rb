9a22a9bb7946a57166c42f0151f8df2b9158711f
Will Offit <will.offit@gmail.com>
2020-06-23 01:01:04 -0700

    Mancala

---
+++ W4D2/Mancala/lib/board.rb
@@ -0,0 +1,87 @@
+require_relative "./player.rb"
+require_relative "./mancala.rb"
+
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) { Array.new }
+    place_stones
+  end
+
+  def place_stones
+    cups.each_with_index do |cup, idx|
+      unless idx == 6 || idx == 13
+        cups[idx] = [:stone, :stone, :stone, :stone]
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    unless (0..13).include?(start_pos)
+      raise "Invalid starting cup"
+    end
+
+    if cups[start_pos].empty?
+      raise "Starting cup is empty"
+    end
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+    until stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+
+    render
+    next_turn(cup_idx)
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif cups[ending_cup_idx].length == 1
+      :switch
+    else
+      ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    (0..5).all? { |idx| cups[idx].empty? } || (7..12).all? { |idx| cups[idx].empty? }
+  end
+
+  def winner
+    if cups[6].size > cups[13].size
+      @name1
+    elsif cups[13].size > cups[6].size
+      @name2
+    else
+      :draw
+    end
+  end
+end
