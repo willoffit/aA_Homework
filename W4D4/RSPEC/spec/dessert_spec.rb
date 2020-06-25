3e6d8f9271e18396bcea1cb9957984f5aac99ea7
Will Offit <will.offit@gmail.com>
2020-06-25 01:22:55 -0700

    W4D4

---
+++ W4D4/RSPEC/spec/dessert_spec.rb
@@ -0,0 +1,69 @@
+require 'rspec'
+require 'dessert'
+
+=begin
+Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
+=end
+
+describe Dessert do
+  subject(:dessert) { Dessert.new("Spaghetti", 5, "Arnold") }
+  let(:chef) { double("chef") }
+
+  describe "#initialize" do
+    it "sets a type" do
+      expect(dessert.type).to eq("Spaghetti")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(5)
+    end
+
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("Spaghetti", "5 lbs", "Arnold") }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe "#add_ingredient" do
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("meatball")
+      expect(dessert.ingredients.last).to eq("meatball")
+    end
+  end
+
+  describe "#mix!" do
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient("meatball")
+      dessert.add_ingredient("sauce")
+      dessert.add_ingredient("cheese")
+      dessert.mix!
+      expect(dessert.ingredients[0]).not_to eql("meatball")
+    end
+  end
+
+  describe "#eat" do
+    it "subtracts an amount from the quantity" do
+      dessert.eat(3)
+      expect(dessert.quantity).to eq(2)
+    end
+
+    it "raises an error if the amount is greater than the quantity" do
+      expect { dessert.eat(6) }.to raise_error("not enough left!")
+    end
+  end
+
+  describe "#serve" do
+    it "contains the titleized version of the chef's name" do
+      expect(dessert.serve).to include("Arnold")
+    end
+  end
+
+  describe "#make_more" do
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake*(dessert))
+    end
+  end
+end
